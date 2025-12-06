455be73a257f6f0cd8744e5158bb3dd41d4ed9b1
sandovalwilkerson705-dotcom <sandovalwilkerson705@gmail.com>
2025-12-06 09:18:34 -0400

    Actualizar infor-ping.js

--- plugins/infor-ping.js
+++ plugins/infor-ping.js
@@ -1,22 +1,17 @@
-import speed from 'performance-now'
-import { spawn, exec, execSync } from 'child_process'
+import speed from "performance-now";
+import { spawn, exec, execSync } from "child_process";
 
 let handler = async (m, { conn }) => {
-  let timestamp = speed()
-  let sentMsg = await conn.reply(m.chat, '❐ Calculando ping...', m)
-  let latency = speed() - timestamp
-
+  let timestamp = speed();
+  let latensi = speed() - timestamp;
   exec(`neofetch --stdout`, (error, stdout, stderr) => {
-    let child = stdout.toString("utf-8")
-    let ssd = child.replace(/Memory:/, "Ram:")
+    let child = stdout.toString("utf-8");
+    let ssd = child.replace(/Memory:/, "Ram:");
+    m.reply(`${ssd}乂  *Speed* : ${latensi.toFixed(4)} _ms_`);
+  });
+};
+handler.help = ["ping"];
+handler.tags = ["info"];
+handler.command = ["ping", "p"];
 
-    let result = `✩ *¡Pong!*\n> *_✦ Tiempo ${latency.toFixed(4).split(".")[0]}ms_*\n${ssd}`
-    conn.sendMessage(m.chat, { text: result, ...rcanal, edit: sentMsg.key }, { quoted: m })
-  })
-}
-
-handler.help = ['ping']
-handler.tags = ['informacion']
-handler.command = ['ping', 'p']
-
-export default handler
+export default handler;
